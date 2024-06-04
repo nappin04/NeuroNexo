99cf7bdadb29e5eb255d001e227593a1bffdcfe6
zorro <noloseaa@>
2024-06-04 18:41:03 +0200

    He terminado el nivel de calculo. He añadido un crono tanto en el nivel de los colores como en el del calculo. Faltan las adivinanzas, los niveles dificiles y ponerlo todo bonito

--- app/src/main/java/com/example/neuronexo/Subniveles/Calculo1.java
+++ app/src/main/java/com/example/neuronexo/Subniveles/Calculo1.java
@@ -1,16 +1,88 @@
 package com.example.neuronexo.Subniveles;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.neuronexo.Models.ToastMensajes;
+import com.example.neuronexo.Niveles.Nivel1;
 import com.example.neuronexo.R;
 
 public class Calculo1 extends AppCompatActivity {
+    private EditText operacion1, operacion2, operacion3, operacion4, operacion5;
+    private Button terminar;
+    private TextView crono;
+    private ToastMensajes mensaje = new ToastMensajes();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculo1);
+        operacion1 = findViewById(R.id.et_valorOp1);
+        operacion2 = findViewById(R.id.et_valorOp2);
+        operacion3 = findViewById(R.id.et_valorOp3);
+        operacion4 = findViewById(R.id.et_valorOp4);
+        operacion5 = findViewById(R.id.et_valorOp5);
+        terminar = findViewById(R.id.bt_terminar);
+        crono = findViewById(R.id.tv_crono2);
+
+        CountDownTimer countDownTimer = new CountDownTimer(60000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                crono.setText("Segundos restantes, "+ millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                crono.setText("¡TIEMPO AGOTADO!");
+                ejercicio(crono);
+            }
+        }.start();
+    }
+
+    public void ejercicio(View v){
+        if(crono.getText().toString().equals("¡TIEMPO AGOTADO!")){
+            AlertDialog.Builder builder = new AlertDialog.Builder(Calculo1.this);
+            builder.setTitle("Se acabó el tiempo")
+                    .setPositiveButton("Terminar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Calculo1.this, Nivel1.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    })
+                    .setCancelable(false).show();
+
+
+        }else{
+            if(operacion1.getText().toString().isEmpty() ||
+                    operacion2.getText().toString().isEmpty() ||
+                    operacion3.getText().toString().isEmpty() ||
+                    operacion4.getText().toString().isEmpty() ||
+                    operacion5.getText().toString().isEmpty()){
+                mensaje.mostrarToastcorto(Calculo1.this, "Debes realizar todas las operaciones");
+
+            }
+            else if(operacion1.getText().toString().equals("11") &&
+                    operacion2.getText().toString().equals("27") &&
+                    operacion3.getText().toString().equals("80") &&
+                    operacion4.getText().toString().equals("169") &&
+                    operacion5.getText().toString().equals("571")){
+
+            }else{
+                mensaje.mostrarToastcorto(Calculo1.this,"Uy, alguno no es correcto");
+
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/neuronexo/Subniveles/Colores1.java
+++ app/src/main/java/com/example/neuronexo/Subniveles/Colores1.java
@@ -1,12 +1,16 @@
 package com.example.neuronexo.Subniveles;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.neuronexo.Models.ToastMensajes;
 import com.example.neuronexo.Niveles.Nivel1;
@@ -15,8 +19,10 @@ import com.example.neuronexo.R;
 public class Colores1 extends AppCompatActivity {
 
     private EditText et1, et2, et3, et4;
+    private TextView crono;
     private ToastMensajes toastMensajes = new ToastMensajes();
-    private int intentos = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +32,56 @@ public class Colores1 extends AppCompatActivity {
         et2 = findViewById(R.id.et2);
         et3 = findViewById(R.id.et3);
         et4 = findViewById(R.id.et4);
+        crono = findViewById(R.id.tv_crono);
+        CountDownTimer countDownTimer = new CountDownTimer(60000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                crono.setText("Segundos restantes: " + millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                crono.setText("¡TIEMPO AGOTADO!");
+                ejercicio(crono);
+            }
+        }.start();
     }
 
     public void ejercicio(View v) {
-        if (et1.getText().toString().isEmpty() ||
-                et2.getText().toString().isEmpty() ||
-                et3.getText().toString().isEmpty() ||
-                et4.getText().toString().isEmpty()) {
+        if(crono.getText().toString().equals("¡TIEMPO AGOTADO!")){
+            AlertDialog.Builder builder = new AlertDialog.Builder(Colores1.this);
+            builder.setTitle("Se acabó el tiempo")
+                    .setPositiveButton("Terminar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Colores1.this, Nivel1.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    })
+                    .setCancelable(false).show();
 
-            toastMensajes.mostrarToastcorto(Colores1.this, "No puedes dejar ninguno vacío");
-        } else if (et1.getText().toString().equals("rojo") &&
-                et2.getText().toString().equals("azul") &&
-                et3.getText().toString().equals("verde") &&
-                et4.getText().toString().equals("amarillo")) {
 
-            toastMensajes.mostrarToastcorto(Colores1.this, "CORRECTO");
-            volver();
-        } else {
-            toastMensajes.mostrarToastcorto(Colores1.this, "Uy, alguno está mal");
+        }else{
+            if (et1.getText().toString().isEmpty() ||
+                    et2.getText().toString().isEmpty() ||
+                    et3.getText().toString().isEmpty() ||
+                    et4.getText().toString().isEmpty()) {
+
+                toastMensajes.mostrarToastcorto(Colores1.this, "No puedes dejar ninguno vacío");
+            } else if (et1.getText().toString().equals("rojo") &&
+                    et2.getText().toString().equals("azul") &&
+                    et3.getText().toString().equals("verde") &&
+                    et4.getText().toString().equals("amarillo")) {
+
+                toastMensajes.mostrarToastcorto(Colores1.this, "CORRECTO");
+                volver();
+            } else {
+                toastMensajes.mostrarToastcorto(Colores1.this, "Uy, alguno está mal");
+            }
         }
-    }
+
+        }
 
     public void volver() {
         finish();
